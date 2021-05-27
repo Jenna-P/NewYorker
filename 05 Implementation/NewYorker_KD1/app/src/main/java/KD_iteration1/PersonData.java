5af07676141cbc36e07f45329db782f604fda578
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-05-27 13:23:42 +0200

    Add files via upload

---
+++ 05 Implementation/NewYorker_KD1/app/src/main/java/KD_iteration1/PersonData.java
@@ -0,0 +1,24 @@
+package KD_iteration1;
+
+public class PersonData {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+
+    public String getName() {
+        //hente fra bruger input
+        return name;
+    }
+
+    public String getEmail() {
+        //hente fra bruger input
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        //hente fra bruger input
+        return phoneNumber;
+    }
+}
