b147179c05c101372875ddc60237270a8e46f377
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-06-02 11:06:14 +0200

    ændret navne

--- 05 Implementation/New-Yorker designer app/app/src/main/java/com/example/newyorkerui/MainActivity.java
+++ 05 Implementation/New-Yorker designer app/app/src/main/java/com/example/newyorkerui/MainActivity.java
@@ -4,20 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button switchButton;
+    Button catalogButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        catalogButton = (Button) findViewById(R.id.catalougeButton);;
+        catalogButton.setOnClickListener(view -> showCatalog());
+
         switchButton = (Button) findViewById(R.id.nextActivityButton);;
         switchButton.setOnClickListener(view -> launchActivity());
+    }
+
+    private void showCatalog() {
+        Intent intent = new Intent(this, Catalogue.class);
+        startActivity(intent);
     }
 
     private void launchActivity() {
