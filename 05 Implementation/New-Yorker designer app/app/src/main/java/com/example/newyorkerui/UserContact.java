7ece223555eb7708df529df5502502dcf3e1770b
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-06-03 20:07:17 +0200

    lavede test sending mail

--- 05 Implementation/New-Yorker designer app/app/src/main/java/com/example/newyorkerui/UserContact.java
+++ 05 Implementation/New-Yorker designer app/app/src/main/java/com/example/newyorkerui/UserContact.java
@@ -3,6 +3,7 @@ package com.example.newyorkerui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.DropBoxManager;
 import android.view.View;
@@ -13,6 +14,11 @@ public class UserContact extends AppCompatActivity {
 
     Button switchButton;
 
+    public EditText uName;
+    public  EditText uEmail;
+    public  EditText uPhone;
+    public  EditText uComments;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +28,10 @@ public class UserContact extends AppCompatActivity {
         switchButton = findViewById(R.id.sendorder);
 
         //brugerens input
-        EditText uName = findViewById(R.id.name);
-        EditText uEmail = findViewById(R.id.email);
-        EditText uPhone = findViewById(R.id.phone);
-        EditText uComments = findViewById(R.id.comments);
+        uName = findViewById(R.id.name);
+        uEmail = findViewById(R.id.email);
+        uPhone = findViewById(R.id.phone);
+        uComments = findViewById(R.id.comments);
         View uMaker = findViewById(R.id.dropdownlist);
 
         switchButton.setOnClickListener(view -> launchActivity());
@@ -33,7 +39,17 @@ public class UserContact extends AppCompatActivity {
 
     private void launchActivity() {
 
-        Intent intentu = new Intent(this, ShowAccept.class);
-        startActivity(intentu);
+       // Intent intentu = new Intent(this, ShowAccept.class);
+      //  startActivity(intentu);
+
+        //test to send mail instead to show confirm activity
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{uEmail.getText().toString()});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Tilbud");
+        intent.putExtra(Intent.EXTRA_TEXT, uComments.getText().toString());
+        // intent.setType("message/rfc822");
+
+        intent.setData(Uri.parse("mailto:"));
+        startActivity(intent);
     }
 }
