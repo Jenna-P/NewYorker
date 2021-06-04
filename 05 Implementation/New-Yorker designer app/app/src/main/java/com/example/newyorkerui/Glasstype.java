de39997d52b03fae4783af758a51e40f24bff666
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-06-04 11:40:33 +0200

    lavede galstype activity

--- 05 Implementation/New-Yorker designer app/app/src/main/java/com/example/newyorkerui/Glasstype.java
+++ 05 Implementation/New-Yorker designer app/app/src/main/java/com/example/newyorkerui/Glasstype.java
@@ -4,25 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Glasstype extends AppCompatActivity {
 
     Button switchButton;
+    RadioGroup radioGroup;
+    RadioButton rb;
+    CheckBox cb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glasstype);
 
+        cb = (CheckBox) findViewById(R.id.checkBox);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         switchButton = (Button) findViewById(R.id.AcceptGlasstype);
         switchButton.setOnClickListener(view -> launchActivity());
+
+        if(cb.isChecked()) {
+            Toast.makeText(this, "Dør valgt ",
+                    Toast.LENGTH_SHORT).show();
+        }
+
     }
+
 
     private void launchActivity() {
 
-        Intent intente = new Intent(this, ShowWall.class);
-        startActivity(intente);
+
+            Intent intente = new Intent(this, ShowWall.class);
+            startActivity(intente);
+
+    }
+
+    public void checkButton(View v) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+
+        rb = findViewById(radioId);
+
     }
 }
